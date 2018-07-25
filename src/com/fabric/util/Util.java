ff9aa5cb0d27648323131db53db5183d3b9e2ca7
Vishal Yadav <vishal3152@gmail.com>
2018-07-25 09:09:43 +0530

    Update Util.java

--- src/com/fabric/util/Util.java
+++ src/com/fabric/util/Util.java
@@ -16,20 +16,25 @@ public class Util {
      * @param userContext
      * @throws Exception
      */
-    public static void writeUserContext(UserContext userContext) throws Exception {
-        String directoryPath = "cred/" + userContext.getAffiliation();
-        String filePath = directoryPath + "/" + userContext.getName() + ".context";
-        File directory = new File(directoryPath);
-        if (!directory.exists())
-            directory.mkdirs();
+    public static void writeUserContext(UserContext userContext) {
+        try {
+            String directoryPath = "cred/" + userContext.getAffiliation();
+            String filePath = directoryPath + "/" + userContext.getName() + ".context";
+            File directory = new File(directoryPath);
+            if (!directory.exists())
+                directory.mkdirs();
 
-        FileOutputStream file = new FileOutputStream(filePath);
-        ObjectOutputStream out = new ObjectOutputStream(file);
+            FileOutputStream file = null;
+            file = new FileOutputStream(filePath);
+            ObjectOutputStream out = new ObjectOutputStream(file);
 
-        out.writeObject(userContext);
-
-        out.close();
-        file.close();
+            out.writeObject(userContext);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            out.close();
+            file.close();
+        }
     }
 
     /**
@@ -40,21 +45,24 @@ public class Util {
      * @return
      * @throws Exception
      */
-    public static UserContext readUserContext(String affiliation, String username) throws Exception {
-        String filePath = "cred/" + affiliation + "/" + username + ".context";
-        File file = new File(filePath);
-        if (file.exists()) {
-            FileInputStream fileStream = new FileInputStream(filePath);
-            ObjectInputStream in = new ObjectInputStream(fileStream);
-
-            UserContext uContext = (UserContext) in.readObject();
-
+    public static UserContext readUserContext(String affiliation, String username) {
+        UserContext uContext = null;
+        try {
+            String filePath = "cred/" + affiliation + "/" + username + ".context";
+            File file = new File(filePath);
+            if (file.exists()) {
+                FileInputStream fileStream = new FileInputStream(filePath);
+                ObjectInputStream in = new ObjectInputStream(fileStream);
+                uContext = (UserContext) in.readObject();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
             in.close();
             fileStream.close();
             return uContext;
         }
 
-        return null;
     }
 
 }
